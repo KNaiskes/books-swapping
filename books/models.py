a14b8ff34bb6fcb4ae851941f9da4c889a2a6fe5
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-09-13 12:12:27 +0300

    Add Owner and Book models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=60)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=75)
+    pub_date = models.DateField()
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
