357e2d26b4277b6c30c00ef0d4735d825746fab5
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-09-14 12:19:26 +0300

    Add Author model

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Owner, Book
+from .models import Owner, Book, Author
 
 admin.site.register(Owner)
 admin.site.register(Book)
+admin.site.register(Author)
--- books/models.py
+++ books/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+    class Meta:
+        ordering = ['last_name']
+
 class Owner(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -10,7 +22,7 @@ class Owner(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=75)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     pub_date = models.DateField()
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
 
