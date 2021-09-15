d38da4081175621dde8f5dd49bf281e6d05c65d8
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-09-15 13:15:44 +0300

    Add index view for books

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Author, Book, Owner
 
-# Create your views here.
+
+def index(request):
+    try:
+        books_list = Book.objects.all()
+    except Book.DoesNotExist:
+        print('error')
+
+    return render(request, 'books/index.html', {'books_list': books_list})
