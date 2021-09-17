8d7bdd7a18e0e87593899cb0683796c104ce1b7b
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-09-17 20:17:45 +0300

    Add basic str test for Book model

--- books/tests/test_models.py
+++ books/tests/test_models.py
@@ -22,3 +22,33 @@ class AuthorTestCase(TestCase):
         self.assertEqual(
             str(author2), '%s %s' % (author2.first_name,author2.last_name)
         )
+
+
+class BookTestCase(TestCase):
+    def setUp(self):
+        from datetime import date
+        today = date.today()
+        author1 = Author.objects.create(
+            first_name='Author1', middle_name='M1',last_name='AuthLast1'
+        )
+
+        author2 = Author.objects.create(
+            first_name='Author2', middle_name='M2',last_name='AuthLast2'
+        )
+
+        book1 = Book.objects.create(title='title_book1', pub_date=today)
+        book1.authors.add(author1)
+
+    def test_str(self):
+        author1 = Author.objects.get(id=1)
+        author2 = Author.objects.get(id=2)
+
+        book1 = Book.objects.get(id=1)
+
+        self.assertEqual(
+            str(book1), '%s' % (book1.title)
+        )
+
+        self.assertEqual(
+            str(author2), '%s %s' % (author2.first_name,author2.last_name)
+        )
