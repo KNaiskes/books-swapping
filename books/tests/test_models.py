48c3fd2f523e50c52e4d88ca81adedd77dc7ecb0
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-09-17 20:52:05 +0300

    Add second testing object for Book model

--- books/tests/test_models.py
+++ books/tests/test_models.py
@@ -28,6 +28,7 @@ class BookTestCase(TestCase):
     def setUp(self):
         from datetime import date
         today = date.today()
+
         author1 = Author.objects.create(
             first_name='Author1', middle_name='M1',last_name='AuthLast1'
         )
@@ -39,16 +40,20 @@ class BookTestCase(TestCase):
         book1 = Book.objects.create(title='title_book1', pub_date=today)
         book1.authors.add(author1)
 
+        book2 = Book.objects.create(title='title_book2', pub_date=today)
+        book2.authors.add(author2)
+
     def test_str(self):
         author1 = Author.objects.get(id=1)
         author2 = Author.objects.get(id=2)
 
         book1 = Book.objects.get(id=1)
+        book2 = Book.objects.get(id=2)
 
         self.assertEqual(
             str(book1), '%s' % (book1.title)
         )
 
         self.assertEqual(
-            str(author2), '%s %s' % (author2.first_name,author2.last_name)
+            str(book2), '%s' % (book2.title)
         )
