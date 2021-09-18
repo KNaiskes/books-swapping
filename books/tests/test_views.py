7005b1916b1b1c2babde0847f517a7cd5b4c5ffd
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-09-18 13:00:24 +0300

    Add  view tests for Author model

--- books/tests/test_views.py
+++ books/tests/test_views.py
@@ -27,3 +27,37 @@ class AuthorListViewTest(TestCase):
         response = self.client.get(reverse('books:authors'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'books/authors.html')
+
+
+class AuthorListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+
+        from datetime import date
+        today = date.today()
+
+        for i in range(2):
+            Author.objects.create(
+                first_name=f'first_name {i}',
+                middle_name=f'middle_name {i}',
+                last_name=f'last_name {i}',
+            )
+
+        book1 = Book.objects.create(
+            title = 'book1',
+            pub_date = today
+        )
+        book1.authors.add(None)
+
+    def test_view_url_location(self):
+        response = self.client.get('/books/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('books:books'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_template(self):
+        response = self.client.get(reverse('books:books'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books/books.html')
