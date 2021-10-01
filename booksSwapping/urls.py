0eed8f1b829ca71af672b23c75d112eab00008e1
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-10-01 18:28:34 +0300

    Add book's view as home(page)

--- booksSwapping/urls.py
+++ booksSwapping/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from books.views import books
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls', namespace='books')),
+
+    path('', books, name='home'),
+    path('home/', books, name='home'),
 ]
