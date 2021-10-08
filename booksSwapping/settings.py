ebd2c6ce364835520725794d421c7d0be115bff3
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-10-08 19:59:56 +0300

    Migrate to PostgreSQL

--- booksSwapping/settings.py
+++ booksSwapping/settings.py
@@ -74,10 +74,24 @@ WSGI_APPLICATION = 'booksSwapping.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+# Settings from Dockerfile
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
